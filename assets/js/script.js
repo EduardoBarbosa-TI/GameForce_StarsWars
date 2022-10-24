78cb4eb35ea495ec1361e4e7f9bf30277c587d97
eduardobarbosa-ti <eduardobb@rede.ulbra.br>
2022-10-23 23:57:23 -0300

    finished project in Star Wars game force

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,37 @@
+const partesDoBoneco = Array.from(document.querySelectorAll(".parte-do-boneco"));
+
 const clickBtn = document.querySelector("#btn");
 
+const chutesEfetuados = []
+const palavra = "uma palavra";
+let errosCometidos = 0;
+const quantidadeMaximaDeErrors = partesDoBoneco.length;
+
+mostrarPalavra()
+
+function mostrarPalavra(){
+    let palavraMostrada = " ";
+
+    for(let i = 0; i < palavra.length; i = i + 1 ){
+        if(chutesEfetuados.includes(palavra[i])){
+            palavraMostrada = palavraMostrada + '<span class="letra">' + palavra[i] + "</span>"
+        } else if(palavra[i] === " ") {
+            palavraMostrada = palavraMostrada + " ";
+        }  else{
+            palavraMostrada = palavraMostrada + '<span class="letra">_</span>';
+        }
+    }
+    document.querySelector(".palavra").innerHTML = palavraMostrada;
+}
 
 clickBtn.addEventListener("click", () => {
-    const chute =  prompt("Qual é o seu Chute");
 
-    const chutesEfetuados = []
-    const palavra = "uma palavra"
+    if(errosCometidos === quantidadeMaximaDeErrors) {
+        alert("Você perdeu !!");
+        return;
+    }
+
+    const chute =  prompt("Qual é o seu Chute");
 
     if(chutesEfetuados.includes(chute)){
         alert("Chute ja efetuado");
@@ -15,18 +41,18 @@ clickBtn.addEventListener("click", () => {
     chutesEfetuados.push(chute);
     
     if(palavra.includes(chute)){
+        mostrarPalavra(); 
+    } else {
+        errosCometidos = errosCometidos + 1;
 
-        let palavraMostrada = "";
-
-        for(let i = 0; i < palavra.length; i - i + 1 ){
-            if(chutesEfetuados.includes(palavra[i])){
-                palavraMostrada = '<span class="letra">' + palavra[i] + '</span>'
-            }  
+        for(let i = 0; i < errosCometidos; i = i + 1){
+            partesDoBoneco[i].classList.add("mostrar");
         }
-        alert(palavraMostrada);
+
+        if(errosCometidos === quantidadeMaximaDeErrors) {
+            alert("Você perdeu !!");
+        } else {
+            alert("Chute errado, tente novamente !!");
+        }
     }
-   
 })
-
-
-
